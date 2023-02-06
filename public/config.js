68f3460387af23ab12599f5834c607f047999b0c
jc_xzh <15160037796@163.com>
2023-02-06 09:47:24 +0800

    多余删除

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.platform = {
     url: 'http://192.168.0.28/', // 接口地址
     title: '福建财通 · 快速搭建平台', // 浏览器标题标题
-    backgroundImage: 'assets/assets/back.png', // 登录页背景图片
+    backgroundImage: 'assets/back.jpg', // 登录页背景图片
     chatFlag: false, //是否开启聊天
     chatUrl: 'http://127.0.0.1:8081', // 聊天地址
     pluginFlag: false, // 文档插件是否开启
