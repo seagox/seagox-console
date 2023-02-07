81bb673b97ddb2cb47bb402e862d08391dae8d54
jc_xzh <15160037796@163.com>
2023-02-07 18:06:57 +0800

    fix

--- public/config.js
+++ public/config.js
@@ -2,8 +2,6 @@ window.platform = {
     url: 'http://192.168.0.28/', // 接口地址
     title: '水母 · 快速搭建平台', // 浏览器标题标题
     backgroundImage: 'assets/back.jpg', // 登录页背景图片
-    chatFlag: false, //是否开启聊天
-    chatUrl: 'http://127.0.0.1:8081', // 聊天地址
     pluginFlag: false, // 文档插件是否开启
     wpsUrl: 'http://192.168.0.246:3889/', // word插件地址
     etUrl: 'https://xxx.com/plugin/et/', // excel插件地址
